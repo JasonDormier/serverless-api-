e1a2cc0f5a3b1067f8db5d9e2712449f05ebd139
JasonDormier <jmdormier@gmail.com>
2021-03-10 20:05:34 -0800

    done

---
+++ deletePeople/index.js
@@ -0,0 +1,31 @@
+const dynamoose = require('dynamoose');
+const peopleModel = require('./people.schema.js.js');
+
+exports.handler = async (event) => {
+
+  let data;
+  let id = event.pathParameters && event.pathParameters.id;
+
+  try {
+
+    //const { name, phone } = JSON.parse(event.body);
+    data = await peopleModel.query('id').eq(id).exec();
+
+    let record = new peopleModel({ id });
+    data = await record.delete();
+
+  } catch (e) {
+    return {
+      statusCode: 500,
+      body: e.message,
+    }
+  }
+
+  let response = {
+    statusCode: 200,
+    body: JSON.stringify(data),
+  }
+
+  return response;
+
+}
